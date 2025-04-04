b7f2e1dcdef2457082f8882fd104f785346787f9
KrishJain <krishjain1903@gmail.com>
2025-04-04 20:58:17 +0530

    Use Ref Hook Example

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
 
 const tempMovieData = [
@@ -186,6 +186,28 @@ function NavBar({children})
 
 function Search ({query, setQuery})
 {
+
+  const inputEl = useRef(null);
+
+  useEffect(function () {
+    function callback(e) {
+
+      if (document.activeElement === inputEl.current) {
+        return;
+      }
+
+      if(e.code === "Enter") {
+
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+    document.addEventListener('keydown', callback);
+    return function() {
+      document.addEventListener('keydown', callback);
+    }
+  }, [setQuery])
+
   return (
     <input
       className="search"
@@ -193,6 +215,7 @@ function Search ({query, setQuery})
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref = {inputEl}
     />
   )
 }
